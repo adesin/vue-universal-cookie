f1015757a9ae41121da36c2d040ecbfc8eec4de7
Anton Desin <anton.desin@gmail.com>
2019-03-19 15:13:23 +0300

    Плагин

---
+++ index.mjs
@@ -0,0 +1,10 @@
+/**
+ * Node.js HTTP/2 Server
+ *
+ * @author Anton Desin anton.desin@gmail.com
+ * @copyright (c) Anton Desin | Интернет-агентство IT People
+ * @link https://itpeople.ru
+ */
+
+export ClientPlugin from "./lib/ClientPlugin";
+export ServerPlugin from './lib/ServerPlugin';
