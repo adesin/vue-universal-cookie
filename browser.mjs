a81dcd49ee97f6dcd85ce494d0fa28e1a9338920
Anton Desin <desin@itpeople.ru>
2020-11-05 12:04:00 +0300

    adding browser entrypoint

---
+++ browser.mjs
@@ -0,0 +1,15 @@
+/**
+ * Universal Cookie for Vue.js
+ *
+ * @author Anton Desin anton.desin@gmail.com
+ * @copyright (c) Anton Desin | Интернет-агентство IT People
+ * @link https://itpeople.ru
+ */
+
+import ClientPlugin from './lib/ClientPlugin.mjs';
+import ServerPlugin from './lib/ServerPlugin.mjs';
+
+export {
+  ClientPlugin,
+  ServerPlugin
+};
